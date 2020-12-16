ce0355ac07f5a0012c9a563d10345b29c2638717
Jim Champagne <jim.c@live.be>
2020-12-16 16:31:25 +0100

    update

--- javascript/menu.js
+++ javascript/menu.js
@@ -7,3 +7,23 @@ let hamburgerMenu = document.querySelector(".hamburger-menu");
 hamburgerMenu.addEventListener("click", function () {
 	header.classList.toggle("menu-open");
 });
+
+const debounce = (fn) => {
+	let frame;
+	return (...params) => {
+		if (frame) {
+			cancelAnimationFrame(frame);
+		}
+		frame = requestAnimationFrame(() => {
+			fn(...params);
+		});
+	};
+};
+
+const storeScroll = () => {
+	document.documentElement.dataset.scroll = window.scrollY;
+};
+
+document.addEventListener("scroll", debounce(storeScroll), { passive: true });
+
+storeScroll();
